12787bbff98a1697603a445abc7bd5b2a8471a43
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-11 23:43:24 +0200

    Fixed benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -10,7 +10,8 @@ class AperturePhotometry:
         for x in range(500):
             self.d[x] = None
 
-    def do_test(self, data_shape, apertures, method, error):
+    def do_test(self, data_shape=None, apertures=None, method=None,
+                subpixels=None, error=None):
         
         data = np.ones(data_shape)
         
@@ -19,53 +20,51 @@ class AperturePhotometry:
         else:
             error = None
             
-        aperture_photometry(data, apertures, method=method, error=error)
+        aperture_photometry(data, apertures, method=method, error=error,
+                            subpixels=subpixels)
 
     def time_basic_center(self):
         """
         Small data, single small aperture
         """
-        aperture_photometry(data_shape=(20, 20),
-                            apertures=CircularAperture((10, 10), 5),
-                            method='center',
-                            error=False)
+        self.do_test(data_shape=(20, 20),
+                     apertures=CircularAperture((10, 10), 5),
+                     method='center',
+                     error=False)
 
     def time_basic_subpixels_1(self):
         """
         Small data, single small aperture
         """
-        aperture_photometry(data_shape=(20, 20),
-                            apertures=CircularAperture((10, 10), 5),
-                            method='subpixel', subpixels=1,
-                            error=False)
+        self.do_test(data_shape=(20, 20),
+                     apertures=CircularAperture((10, 10), 5),
+                     method='subpixel', subpixels=1,
+                     error=False)
 
     def time_basic_subpixels_5(self):
         """
         Small data, single small aperture
         """
-        aperture_photometry(data_shape=(20, 20),
-                            apertures=CircularAperture((10, 10), 5),
-                            method='subpixel', subpixels=5,
-                            error=False)
+        self.do_test(data_shape=(20, 20),
+                     apertures=CircularAperture((10, 10), 5),
+                     method='subpixel', subpixels=5,
+                     error=False)
 
     def time_basic_subpixels_10(self):
         """
         Small data, single small aperture
         """
-        aperture_photometry(data_shape=(20, 20),
-                            apertures=CircularAperture((10, 10), 5),
-                            method='subpixel', subpixels=10,
-                            error=False)
+        self.do_test(data_shape=(20, 20),
+                     apertures=CircularAperture((10, 10), 5),
+                     method='subpixel', subpixels=10,
+                     error=False)
 
     def time_basic_exact(self):
         """
         Small data, single small aperture
         """
-        aperture_photometry(data_shape=(20, 20),
-                            apertures=CircularAperture((10, 10), 5),
-                            method='exact',
-                            error=False)
+        self.do_test(data_shape=(20, 20),
+                     apertures=CircularAperture((10, 10), 5),
+                     method='exact',
+                     error=False)
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
